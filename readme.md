0f5fafbec66b1f66967720f2cbb571b8359304ba
warapong-pj <warapong.pj@gmail.com>
2021-05-08 01:50:12 +0000

    export vscode extensions

---
+++ readme.md
@@ -0,0 +1,4 @@
+### how to export vscode extensions
+```
+code --list-extensions | xargs -L 1 echo code --install-extension
+```
---
+++ vscode.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+code --install-extension Extensions installed on SSH: dev:
+code --install-extension golang.go
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-toolsai.jupyter
+code --install-extension octref.vetur
+code --install-extension sdras.vue-vscode-snippets
