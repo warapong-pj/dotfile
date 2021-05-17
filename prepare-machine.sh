bb391088ac13eaaaeacdc954588267704a7a32db
warapong-pj <warapong.pj@gmail.com>
2021-05-17 22:28:25 +0700

    add cloudformation extension

--- prepare-machine.sh
+++ prepare-machine.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-code --install-extension Extensions installed on SSH: dev:
+set -e
+
+code --install-extension aws-scripting-guy.cform
 code --install-extension golang.go
 code --install-extension ms-azuretools.vscode-docker
 code --install-extension ms-python.python
 code --install-extension ms-python.vscode-pylance
 code --install-extension ms-toolsai.jupyter
 code --install-extension octref.vetur
+code --install-extension redhat.vscode-yaml
 code --install-extension sdras.vue-vscode-snippets
